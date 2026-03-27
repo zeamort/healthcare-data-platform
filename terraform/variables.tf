793e810ddcbde624fd1d7b389b5a4206782a6fff
Morteza Rahimi <zeamort@gmail.com>
2026-03-27 15:12:01 -0700

    terraform: add input variables with sensitive db_password

---
+++ terraform/variables.tf
@@ -0,0 +1,49 @@
+# Input variables for the Healthcare Data Platform infrastructure.
+# Sensitive values (db_password) should be provided via terraform.tfvars or TF_VAR_ env vars.
+
+variable "aws_region" {
+  description = "AWS region for resources"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "environment" {
+  description = "Environment name (dev, staging, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "db_username" {
+  description = "Master username for RDS and Redshift"
+  type        = string
+  default     = "postgres"
+}
+
+variable "db_password" {
+  description = "Master password for RDS and Redshift. Provide via TF_VAR_db_password or terraform.tfvars"
+  type        = string
+  sensitive   = true
+}
+
+variable "rds_instance_class" {
+  description = "RDS instance type"
+  type        = string
+  default     = "db.t3.micro"
+}
+
+variable "redshift_node_type" {
+  description = "Redshift node type"
+  type        = string
+  default     = "ra3.xlplus"
+}
+
+variable "redshift_number_of_nodes" {
+  description = "Number of Redshift nodes"
+  type        = number
+  default     = 1
+}
+
+variable "allowed_cidr_blocks" {
+  description = "CIDR blocks allowed to access databases (set to your IP for security, e.g. [\"203.0.113.5/32\"])"
+  type        = list(string)
+}
